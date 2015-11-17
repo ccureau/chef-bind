eddc3ac3c4d42c6d815e1b52c7a967a075732fc1
Chris Cureau <cmcureau@gmail.com>
2015-11-17 06:22:29 -0600

    forward zone template changes

--- providers/zone.rb
+++ providers/zone.rb
@@ -1,11 +1,15 @@
+hosts = Hash.new()
+
 action :create do
-    template "db.#{node['bind']['zonedir']}#{[new_resource.domain]} do
+    template "#{node['bind']['zonedir']}db.#{new_resource.domain}" do
       source "zone.erb"
+      cookbook "bind"
       owner node['bind']['user']
       group node['bind']['group']
       mode 0600
-      variables (
-        :new_resource => new_resource
+      variables(
+        :new_resource => new_resource,
+        :hosts => hosts
       )
       notifies :restart, resources(:service => node['bind']['service_name'])
     end
--- recipes/test_bind_domain.rb
+++ recipes/test_bind_domain.rb
@@ -7,11 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include 'bind::default'
+include_recipe 'bind::default'
 
 bind_zone "cureau.net" do
   admin_email "root.localhost"
-  nameserver "ns1.cureau.net"
+  nameserver [ "ns1.cureau.net" ]
+  mailserver "mail.cureau.net"
   type "master"
   cidr "10.5.0.0/24"
 end
